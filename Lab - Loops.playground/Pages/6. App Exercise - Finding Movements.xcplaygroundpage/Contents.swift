dce4f16a0b5ce42572b1b5dec73a5755e7983499
Jose Trujillo <joseh.trujillo11@gmail.com>
2023-10-17 13:50:55 -0400

    DONE

--- Lab - Loops.playground/Pages/6. App Exercise - Finding Movements.xcplaygroundpage/Contents.swift
+++ Lab - Loops.playground/Pages/6. App Exercise - Finding Movements.xcplaygroundpage/Contents.swift
@@ -12,7 +12,11 @@ let lowHR = 110
 let highHR = 125
 var movementHeartRates: [String: Int] = ["Walking": 85, "Running": 120, "Swimming": 130, "Cycling": 128, "Skiing": 114, "Climbing": 129]
 
-
+for(moves, HR) in movementHeartRates {
+    if HR > lowHR && HR < highHR {
+        print("you could go \(moves)")
+    }
+}
 /*:
  _Copyright © 2021 Apple Inc._
 
